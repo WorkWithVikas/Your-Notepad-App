61f53d3892919f089bae696f446e48a6049314d4
Vikas Rathore <jarvis.90v@gmail.com>
2025-01-17 12:02:46 +0530

    Add files via upload

---
+++ YourNotePad/run-YourNotepad.bat
@@ -0,0 +1,3 @@
+@echo off
+start javaw --module-path "C:\Users\HP\Downloads\openjfx-23.0.1_windows-x64_bin-sdk\javafx-sdk-23.0.1\lib" --add-modules javafx.controls,javafx.fxml -jar YourNotepad.jar
+exit
